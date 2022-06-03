38c191c8e9d2ca2ef0275a7742d92df598d45c31
H4O <github@h4o.dev>
2022-06-03 19:36:44 -0400

    fix: bypass git hooks by default (#47)
    * Bypass git hooks by default
    
    - `npm publish` doesn't run git hooks, gitpkg shouldn't either
    
    Signed-off-by: haoadoresorange <github@comtam.dev>
    
    * style: fix some quotes, styles
    
    Co-authored-by: haoadoresorange <github@comtam.dev>
    Co-authored-by: Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>

--- src/tasks/Publish/upload-package.js
+++ src/tasks/Publish/upload-package.js
@@ -9,16 +9,19 @@ export default async function uploadPackage(config, pkg, registry) {
   const gitpkgPackageName = getGitTagName(pkg, config);
   await execLikeShell('git init', pkgTempDirPkg);
   await execLikeShell('git add .', pkgTempDirPkg);
-  await execLikeShell('git commit -m gitpkg', pkgTempDirPkg);
+  await execLikeShell('git commit --no-verify -m gitpkg', pkgTempDirPkg);
   await execLikeShell(`git remote add origin ${registry}`, pkgTempDirPkg);
   await execLikeShell(`git tag ${gitpkgPackageName}`, pkgTempDirPkg);
   try {
     await execLikeShell(`git push origin ${gitpkgPackageName}`, pkgTempDirPkg);
   } catch (err) {
-    const gitErrorExists = 'Updates were rejected because the tag already exists in the remote.';
+    const gitErrorExists =
+      'Updates were rejected because the tag already exists in the remote.';
     const exists = err.stderr.indexOf(gitErrorExists) > -1;
     if (exists) {
-      throw new Error(`The git tag "${gitpkgPackageName}" already exists in "${registry}".`);
+      throw new Error(
+        `The git tag "${gitpkgPackageName}" already exists in "${registry}".`
+      );
     }
 
     throw err;

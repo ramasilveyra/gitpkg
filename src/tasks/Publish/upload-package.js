c2c08417398f6946fb935f304e3415638b482127
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2022-06-03 20:35:01 -0300

    style: fix some quotes, styles

--- src/tasks/Publish/upload-package.js
+++ src/tasks/Publish/upload-package.js
@@ -1,22 +1,22 @@
-import path from "path";
-import execLikeShell from "./exec-like-shell";
-import getTempDir from "./get-temp-dir";
-import getGitTagName from "./get-git-tag-name";
+import path from 'path';
+import execLikeShell from './exec-like-shell';
+import getTempDir from './get-temp-dir';
+import getGitTagName from './get-git-tag-name';
 
 export default async function uploadPackage(config, pkg, registry) {
   const pkgTempDir = await getTempDir(pkg);
-  const pkgTempDirPkg = path.join(pkgTempDir, "package");
+  const pkgTempDirPkg = path.join(pkgTempDir, 'package');
   const gitpkgPackageName = getGitTagName(pkg, config);
-  await execLikeShell("git init", pkgTempDirPkg);
-  await execLikeShell("git add .", pkgTempDirPkg);
-  await execLikeShell("git commit --no-verify -m gitpkg", pkgTempDirPkg);
+  await execLikeShell('git init', pkgTempDirPkg);
+  await execLikeShell('git add .', pkgTempDirPkg);
+  await execLikeShell('git commit --no-verify -m gitpkg', pkgTempDirPkg);
   await execLikeShell(`git remote add origin ${registry}`, pkgTempDirPkg);
   await execLikeShell(`git tag ${gitpkgPackageName}`, pkgTempDirPkg);
   try {
     await execLikeShell(`git push origin ${gitpkgPackageName}`, pkgTempDirPkg);
   } catch (err) {
     const gitErrorExists =
-      "Updates were rejected because the tag already exists in the remote.";
+      'Updates were rejected because the tag already exists in the remote.';
     const exists = err.stderr.indexOf(gitErrorExists) > -1;
     if (exists) {
       throw new Error(

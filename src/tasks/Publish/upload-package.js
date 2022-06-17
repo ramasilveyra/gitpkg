17d3f85b27cbe5b4a05704392046d12d1a4fc914
Brenton Simpson <appsforartists@users.noreply.github.com>
2022-06-17 14:09:00 -0700

    fix: make gitpkg publish idempotent (#62)
    * fix: make gitpkg publish idempotent
    
    By pinning the commit and author dates, SHAs become deterministic from the file contents alone.  This prevents `git push origin <tag>` from failing, unless the underlying files don't match.
    
    As written, this behavior becomes the new default.  If we envision problems, we could add a flag that enables/disables it.
    
    Fixes #60

--- src/tasks/Publish/upload-package.js
+++ src/tasks/Publish/upload-package.js
@@ -4,6 +4,16 @@ import getTempDir from './get-temp-dir';
 import getGitTagName from './get-git-tag-name';
 
 export default async function uploadPackage(config, pkg, registry) {
+  // Timestamps are one of the inputs that cause entropy in a commit SHA.  By
+  // freezing the timestamps, we ensure that running `gitpkg publish` twice on
+  // the same files results in the same SHA.
+  //
+  // This makes publishing idempotent: you can run `gitpkg publish` as many
+  // times as you like, and it won't error unless the underlying files don't
+  // match.
+  process.env.GIT_AUTHOR_DATE = `1970-01-01T00:00:00.000Z`;
+  process.env.GIT_COMMITTER_DATE = `1970-01-01T00:00:00.000Z`;
+
   const pkgTempDir = await getTempDir(pkg);
   const pkgTempDirPkg = path.join(pkgTempDir, 'package');
   const gitpkgPackageName = getGitTagName(pkg, config);
@@ -12,6 +22,23 @@ export default async function uploadPackage(config, pkg, registry) {
   await execLikeShell('git commit --no-verify -m gitpkg', pkgTempDirPkg);
   await execLikeShell(`git remote add origin ${registry}`, pkgTempDirPkg);
   await execLikeShell(`git tag ${gitpkgPackageName}`, pkgTempDirPkg);
+
+  // This command looks up the existing tags on the remote.
+  //
+  // If you push a tag that already exists, it should succeed with
+  // "Everything up-to-date".  However, your local copy must know the remote
+  // has the matching tag.  Otherwise, you'll receive `gitErrorExists`.
+  //
+  // By fetching and then pushing, we ensure that:
+  // -  If the tag hasn't been pushed yet, it is uploaded and the command
+  //    succeeds.
+  // -  If an identical tag has already been pushed, the command succeeds with
+  //    "Everything up-to-date".
+  // -  If the same tag name has already been pushed with different contents
+  //    (e.g. you forgot to change the version after making changes), the
+  //    push is rejected and an error is thrown.
+  await execLikeShell(`git fetch origin 'refs/tags/*:*'`, pkgTempDirPkg);
+
   try {
     await execLikeShell(`git push origin ${gitpkgPackageName}`, pkgTempDirPkg);
   } catch (err) {
